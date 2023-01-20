6766bcc2827d3bfd0147acedc0f3de652bc6ba76
Asucan <76811993+Asucanyh-cn@users.noreply.github.com>
2023-01-20 13:17:55 +0800

    Update index.py
    修复页面改版后，正则匹配失效问题。

--- api/index.py
+++ api/index.py
@@ -10,10 +10,16 @@ def getdata(name):
     gitpage = requests.get("https://github.com/" + name)
     data = gitpage.text
     datadatereg = re.compile(r'data-date="(.*?)" data-level')
-    datacountreg = re.compile(r'data-count="(.*?)" data-date')
+    # 修复页面改版后，正则匹配失效问题 1 
+    # github页面改版前
+    # datacountreg = re.compile(r'data-count="(.*?)" data-date')   
+    datacountreg = re.compile(r'rx="2" ry="2">(.*?) contribution')
     datadate = datadatereg.findall(data)
     datacount = datacountreg.findall(data)
-    datacount = list(map(int, datacount))
+    # 修复页面改版后，正则匹配失效问题 2
+    # github页面改版前
+    # datacount = list(map(int, datacount))
+    datacount = list(map(int, [0 if i == "No" else i for i in datacount]))
     contributions = sum(datacount)
     datalist = []
     for index, item in enumerate(datadate):
