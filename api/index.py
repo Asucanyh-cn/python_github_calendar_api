ff6b9812e48c400671c8b861d85ed04a3e83613a
Asucan <76811993+Asucanyh-cn@users.noreply.github.com>
2023-07-28 18:25:59 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -13,14 +13,16 @@ def getdata(name):
     # 修复页面改版后，正则匹配失效问题 1 
     # github页面改版前
     # datacountreg = re.compile(r'data-count="(.*?)" data-date')   
-    datacountreg = re.compile(r'rx="2" ry="2">(.*?) contribution')
+    # datacountreg = re.compile(r'rx="2" ry="2">(.*?) contribution')
+    datacountreg = re.compile(r'<span class="sr-only">(\w*) contribution')
     datadate = datadatereg.findall(data)
+    print(len(datadate))
     datacount = datacountreg.findall(data)
+    print(len(datacount))
     # 修复页面改版后，正则匹配失效问题 2
     # github页面改版前
     # datacount = list(map(int, datacount))
     # datacount = list(map(int, [0 if i == "No" else i for i in datacount]))
-    datacountreg = re.compile(r'<span class="sr-only">(\w*) contribution')
     contributions = sum(datacount)
     datalist = []
     for index, item in enumerate(datadate):
